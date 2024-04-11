efc6bfc12b816e5301a9061f6a79af5fc67163ca
Parichay Mago <parichaymago@gmail.com>
2024-04-11 10:51:11 +0530

    2.0-urc22punjab

---
+++ types/login.ts
@@ -0,0 +1,12 @@
+export interface Login {
+  name: String,
+  email: String,
+  password: String
+}
+
+
+export interface Signup {
+  name: String,
+  rationCardNumber: String,
+  password: String
+}
